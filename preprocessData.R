e0ffe79a1a9c885bd3862dce0e2e8a8d0ff51b45
dyammons <dyammons95@gmail.com>
2024-02-01 07:55:12 -0700

    Run: code used to integrate data using log norm
    This script was used to integrate data using log normlaization.
    Based on previous analysis this appears to be the best approach.
    
    Files: allCells_S3.rds allCellslabTransfer_S3.rds bm_cd34_analysis_240201_log_integrated_S2.rds
    JOBID: 4568575

--- preprocessData.R
+++ preprocessData.R
@@ -8,8 +8,8 @@ source("/pl/active/dow_lab/dylan/repos/scrna-seq/analysis-code/customFunctions_S
 ######### MODIFY #########
 
 #set output name -- recommend including data and sample size
-experiment <- "bm_cd34_analysis_240131"
-outName <- "allCells_SCT"
+experiment <- "bm_cd34_analysis_240201_log"
+outName <- "allCells"
 
 nFeature_RNA_high <- 5500
 nFeature_RNA_low <- 100
@@ -21,22 +21,20 @@ nCount_RNA_low <- 200
 
 # load10x(din = "../input/", dout = "../output/s1/", outName = experiment, testQC = FALSE, removeRBC_pal = FALSE,
 #         nFeature_RNA_high = nFeature_RNA_high, nFeature_RNA_low = nFeature_RNA_low, percent.mt_high = percent.mt_high, 
-#         nCount_RNA_high = nCount_RNA_high, nCount_RNA_low = nCount_RNA_low)
+#        nCount_RNA_high = nCount_RNA_high, nCount_RNA_low = nCount_RNA_low)
 
 #integrate the data using all of the four Seurat v5 integration methods
-seu.obj <- integrateData(din = "../output/s1/", dout = "../output/s2/", outName = experiment, normalization.method = "SCT",
-                         runAllMethods = TRUE, indReClus = F)
+seu.obj <- integrateData(din = "../output/s1/", dout = "../output/s2/", outName = experiment, runAllMethods = TRUE)
 
 # #use clustree to identify clustering parameters that appear most appropriate
 # clusTree(seu.obj = seu.obj, dout = "../output/clustree/", outName = experiment, 
 #             test_dims = c(50,45,40), algorithm = 3, prefix = "integrated_snn_res.")
 
-
 #complete data visualization
 for (x in list("integrated.cca", "integrated.harmony", "integrated.joint", "integrated.rcpa")) {
     seu.obj <- dataVisUMAP(seu.obj = seu.obj, outDir = "../output/s3/", outName = paste0(outName, "_", x), 
-                           final.dims = 30, final.res = 0.6, stashID = "clusterID", algorithm = 3, min.dist = 0.1, n.neighbors = 10,
-                           prefix = "SCT_snn_res.", assay = "SCT", reduction = x,
+                           final.dims = 45, final.res = 0.6, stashID = "clusterID", algorithm = 3, min.dist = 0.1, n.neighbors = 10,
+                           prefix = "RNA_snn_res.", assay = "RNA", reduction = x,
                            saveRDS = F, return_obj = T, returnFeats = T,
                            features = c("PTPRC", "CD3E", "CD8A", "GZMA", 
                                         "IL7R", "ANPEP", "FLT3", "DLA-DRA", 
@@ -44,9 +42,7 @@ for (x in list("integrated.cca", "integrated.harmony", "integrated.joint", "inte
                           )
 }
 
-
 saveRDS(seu.obj, paste0("../output/s3/", outName,"_S3.rds"))
-
 
 #check QC params
 features <- c("nCount_RNA", "nFeature_RNA", "percent.mt")
@@ -105,7 +101,7 @@ p <- formatUMAP(plot = pi) + NoLegend()
 ggsave(paste0("../output/", outName, "/", outName, "_minorIdent_UMAP.png"), width = 14, height = 7)
 
 #save the processed object
-saveRDS(seu.obj, "../output/s3/bm_cd34_analysis_231211_v5_integrated_res0.6_dims45_dist0.1_neigh10_S3.rds")
+saveRDS(seu.obj, paste0("../output/s3/", outName,"labTransfer_S3.rds"))
 
 
 
