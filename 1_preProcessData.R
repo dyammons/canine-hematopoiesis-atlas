9572bcef5d7ae5f594f412aa48ce2a010167cc12
dyammons <dyammons95@gmail.com>
2024-02-01 15:48:51 -0700

    Clean: rename preprocessData.R -> 1_preProcessData.R

--- 1_preProcessData.R
+++ 1_preProcessData.R
@@ -1,7 +1,7 @@
 #!/usr/bin/Rscript
 
 #load custom functions & packages
-source("/pl/active/dow_lab/dylan/repos/scrna-seq/analysis-code/customFunctions_Seuratv5.R")
+source("/pl/active/CSUClinHeme/users/dylan/repos/scrna-seq/analysis-code/customFunctions_Seuratv5.R")
 
 ##### prepare data set #####
 
