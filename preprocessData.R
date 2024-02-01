aa3d13b8f90ad10fdf85346e2c4c5622191b1902
dyammons <dyammons95@gmail.com>
2024-01-31 18:07:01 -0700

    modify preprocessData.R to use SCT normalization rather than log norm

--- preprocessData.R
+++ preprocessData.R
@@ -1,14 +1,14 @@
 #!/usr/bin/Rscript
 
 #load custom functions & packages
-source("./customFunctions_Seuratv5.R")
+source("/pl/active/dow_lab/dylan/repos/scrna-seq/analysis-code/customFunctions_Seuratv5.R")
 
 ##### prepare data set #####
 
 ######### MODIFY #########
 
 #set output name -- recommend including data and sample size
-experiment <- "bm_cd34_analysis_102723"
+experiment <- "bm_cd34_analysis_240131"
 outName <- "allCells"
 
 nFeature_RNA_high <- 5500
@@ -19,14 +19,13 @@ nCount_RNA_low <- 200
 
 ########## END MODIFY #########
 
-load10x(din = "../input/", dout = "../output/s1/", outName = experiment, testQC = FALSE, removeRBC_pal = FALSE,
-        nFeature_RNA_high = nFeature_RNA_high, nFeature_RNA_low = nFeature_RNA_low, percent.mt_high = percent.mt_high, 
-        nCount_RNA_high = nCount_RNA_high, nCount_RNA_low = nCount_RNA_low)
+# load10x(din = "../input/", dout = "../output/s1/", outName = experiment, testQC = FALSE, removeRBC_pal = FALSE,
+#         nFeature_RNA_high = nFeature_RNA_high, nFeature_RNA_low = nFeature_RNA_low, percent.mt_high = percent.mt_high, 
+#         nCount_RNA_high = nCount_RNA_high, nCount_RNA_low = nCount_RNA_low)
 
 #integrate the data using all of the four Seurat v5 integration methods
-seu.obj <- integrateData(din = "../output/s1/", dout = "../output/s2/", outName = "bm_cd34_analysis_231211_v5", runAllMethods = TRUE)
-
-seu.obj <- readRDS("../output/s2/bm_cd34_analysis_231211_v5_integrated_S2.rds")
+seu.obj <- integrateData(din = "../output/s1/", dout = "../output/s2/", outName = experiment, normalization.method = "SCT",
+                         runAllMethods = TRUE, indReClus = F)
 
 # #use clustree to identify clustering parameters that appear most appropriate
 # clusTree(seu.obj = seu.obj, dout = "../output/clustree/", outName = experiment, 
@@ -34,44 +33,19 @@ seu.obj <- readRDS("../output/s2/bm_cd34_analysis_231211_v5_integrated_S2.rds")
 
 
 #complete data visualization
-seu.obj <- dataVisUMAP(seu.obj = seu.obj, outDir = "../output/s3/", outName = "integrated.cca", 
-                        final.dims = 45, final.res = 0.6, stashID = "clusterID", algorithm = 3, min.dist = 0.1, n.neighbors = 10,
-                        prefix = "RNA_snn_res.", assay = "RNA", reduction = "integrated.cca",
-                        saveRDS = F, return_obj = T, returnFeats = T,
-                        features = c("PTPRC", "CD3E", "CD8A", "GZMA", 
+for (x in list("integrated.cca", "integrated.harmony", "integrated.joint", "integrated.rcpa")) {
+    seu.obj <- dataVisUMAP(seu.obj = seu.obj, outDir = "../output/s3/", outName = x, 
+                           final.dims = 30, final.res = 0.6, stashID = "clusterID", algorithm = 3, min.dist = 0.1, n.neighbors = 10,
+                           prefix = "SCT_snn_res.", assay = "SCT", reduction = x,
+                           saveRDS = F, return_obj = T, returnFeats = T,
+                           features = c("PTPRC", "CD3E", "CD8A", "GZMA", 
                                         "IL7R", "ANPEP", "FLT3", "DLA-DRA", 
                                         "CD4", "MS4A1", "PPBP","HBM")
-)
+                          )
+}
 
-#complete data visualization
-seu.obj <- dataVisUMAP(seu.obj = seu.obj, outDir = "../output/s3/", outName = "integrated.harmony", 
-                        final.dims = 45, final.res = 0.6, stashID = "clusterID", algorithm = 3, min.dist = 0.1, n.neighbors = 10,
-                        prefix = "RNA_snn_res.", assay = "RNA", reduction = "integrated.harmony",
-                        saveRDS = F, return_obj = T, returnFeats = T,
-                        features = c("PTPRC", "CD3E", "CD8A", "GZMA", 
-                                        "IL7R", "ANPEP", "FLT3", "DLA-DRA", 
-                                        "CD4", "MS4A1", "PPBP","HBM")
-)
 
-#complete data visualization
-seu.obj <- dataVisUMAP(seu.obj = seu.obj, outDir = "../output/s3/", outName = "integrated.joint", 
-                        final.dims = 45, final.res = 0.6, stashID = "clusterID", algorithm = 3, min.dist = 0.1, n.neighbors = 10,
-                        prefix = "RNA_snn_res.", assay = "RNA", reduction = "integrated.joint",
-                        saveRDS = F, return_obj = T, returnFeats = T,
-                        features = c("PTPRC", "CD3E", "CD8A", "GZMA", 
-                                        "IL7R", "ANPEP", "FLT3", "DLA-DRA", 
-                                        "CD4", "MS4A1", "PPBP","HBM")
-)
-
-#complete data visualization
-seu.obj <- dataVisUMAP(seu.obj = seu.obj, outDir = "../output/s3/", outName = "integrated.rcpa", 
-                        final.dims = 45, final.res = 0.6, stashID = "clusterID", algorithm = 3, min.dist = 0.1, n.neighbors = 10,
-                        prefix = "RNA_snn_res.", assay = "RNA", reduction = "integrated.rcpa",
-                        saveRDS = F, return_obj = T, returnFeats = T,
-                        features = c("PTPRC", "CD3E", "CD8A", "GZMA", 
-                                        "IL7R", "ANPEP", "FLT3", "DLA-DRA", 
-                                        "CD4", "MS4A1", "PPBP","HBM")
-)
+saveRDS(seu.obj, paste0("../output/s3/", outName,"_S3.rds"))
 
 
 #check QC params
