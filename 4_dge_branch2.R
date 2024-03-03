cc5e121d4f0fe46bf70fb534069a759d2bf52581
dyammons <dyammons95@gmail.com>
2024-03-03 15:04:20 -0700

    Add GEX over pseudotime

--- 4_dge_branch2.R
+++ 4_dge_branch2.R
@@ -23,11 +23,13 @@ seu.obj <- readRDS("../output/s3/allCells_clean_highRes_integrated.harmony_res1.
 cds <- readRDS("../output/s3/240220_CDS_bm_cd34_removed_disconnected.rds")
 ptime <- pseudotime(cds, reduction_method = "UMAP")
 seu.obj$ptime <- ptime
+colz.df <- read.csv("./metaData/allCells_ID_disconected_highRes.csv")
 
 #plot the high-res clustering results
 pi <- DimPlot(seu.obj, 
               reduction = reduction, 
               group.by = "clusterID2_integrated.harmony",
+              cols = colz.df$majCol,
               pt.size = 0.1,
               label = T,
               label.box = T,
@@ -127,7 +129,7 @@ ggsave(paste0("../output/", outName, "/", branch, "/", branch, "_pseudo_highligh
 
 #highlight region by cluster
 colArray <- as.data.frame(matrix(levels(seu.obj@meta.data[ ,groupBy]), dimnames = list(NULL, "clusID")))
-colArray$color <- gg_color_hue(nrow(colArray))
+colArray$color <- colz.df$majCol
 colArray <- colArray %>% mutate(high_col = ifelse(clusID %in% c(base_clus, divergent_clus), color,"grey"))
 
 pi <- DimPlot(seu.obj, 
@@ -138,90 +140,48 @@ pi <- DimPlot(seu.obj,
         label = F,
         label.box = F
  )
-umapHighLight <- formatUMAP(pi) + NoLegend()
+umapHighLight <- formatUMAP(pi) + theme(axis.title = element_blank(),
+                             panel.border = element_blank(),
+                             plot.margin = unit(c(-7, -7, -7, -7), "pt")
+                            ) + NoLegend()
 ggsave(paste0("../output/", outName, "/", branch, "_highlight_UMAP.png"), width = 7, height = 7)
 
+#pick out TFs to highlight
+files <- list.files(path = paste0("../output/", outName, "/"), pattern=".csv", all.files=FALSE,
+                        full.names=T)
+df.list <- lapply(files, read.csv, header = T)
+df <- do.call(rbind, df.list)
 
-#order cells by time then plot the genes in heatmap
-seu.obj.sub <- subset(seu.obj.sub, downsample = min(table(droplevels(seu.obj@meta.data[ ,groupBy]))))
-seu.obj.sub <- ScaleData(seu.obj.sub)
-mat_scaled <- as.matrix(seu.obj.sub@assays$RNA$scale.data) #use scale data
+tfs <- read.csv("./metaData/allTFs_hg38.txt", col.names = "tfs")
 
-order.df <- seu.obj.sub@meta.data[ ,c(groupBy, "ptime")] 
-colnames(order.df)[1] <- "clus"                                                                              
-left.cells <- order.df %>% filter(clus == divergent_clus[2]) %>% arrange(desc(ptime)) %>% rownames()
-center.cells <- order.df %>% filter(clus == base_clus)  %>% rownames()
-right.cells <- order.df %>% filter(clus == divergent_clus[1]) %>% arrange(desc(ptime)) %>% rownames()
+df <- df[df$gene %in% tfs$tfs, ]
 
-#scale data
-mat_scaled <- mat_scaled[rownames(mat_scaled) %in% feats_forHeat,]
-mat_scaled <- mat_scaled[ ,match(c(left.cells,center.cells,right.cells), colnames(mat_scaled))]
+#gene expression by pseudotime
+features <- c("SLPI","CAMP","TREM1", "LGALS1") #only works with 1 feat rn
+colorBy <- "clusterID2_integrated.harmony" #seu metadata slot to color data by
+plotBy <- "ptime" #pseudotime data to use on the x axis -- match with the name of a metadata slot
+clus <- c("17","8")
+rmZeros <- F #option to remove zero values - probablaly can keep it at false
+bin.size <- 5
 
-#prep annotations
-samp <- unique(seu.obj.sub$colz)
-names(samp) <- unique(seu.obj.sub$name)
-ha = HeatmapAnnotation(
-    Sample = unlist(lapply(rownames(mat), function(x){strsplit(x,"--")[[1]][2]})),
-    Cluster = unlist(lapply(rownames(mat), function(x){strsplit(x,"--")[[1]][1]})),
-    border = TRUE,
-    col = list(Sample = samp)
-)
+seu.obj.sub <- subset(seu.obj, clusterID2_integrated.harmony %in% clus)
+mulit.plot <- lapply(features, function(x){
+    df <- FetchData(object = seu.obj.sub, vars = c('cellSource', 'name', colorBy, plotBy, x), layer = "data")
+    colnames(df)[3] <- "cluster"
+    colnames(df)[5] <- "feature"
 
-#plot the data -- this does not work as desired
-png(file = paste0("../output/", outName, "/", branch, "_Heat_degs.png"), width=3000, height=4000, res=400)
-par(mfcol=c(1,1))         
-ht <- Heatmap(mat_scaled, #name = "mat", #col = col_fun,
-              name = "Scaled expression",
-              cluster_rows = F,
-              show_row_names=T,
-              show_column_names=F,
-#               top_annotation = ha,
-              col=viridis(option = "magma",100),
-              cluster_columns = F
-             )
-draw(ht, padding = unit(c(2, 12, 2, 5), "mm"))
-dev.off()
+    if(rmZeros){
+        df <- df[df[ ,"feature"] != 0, ]
+    }
 
-#plot the data using Seurat DoHeatmap
-Idents(seu.obj.sub) <- factor(droplevels(seu.obj.sub@meta.data[ ,groupBy]), levels =  c(divergent_clus[2], base_clus, divergent_clus[1]))
-p <- DoHeatmap(seu.obj.sub, cells = c(left.cells,center.cells,right.cells), features = feats_forHeat)
-ggsave(paste0("../output/", outName, "/", branch, "_seurat_heat.png"), width = 7, height = 7)
+    df <- df %>% na.omit() %>% group_by(cluster) %>% mutate(feat = x,
+                                                            ptime = (ptime - sort(ptime)[3]) / (rev(sort(ptime))[3] - sort(ptime)[3]) 
+                                                           ) %>% arrange(ptime) %>% mutate(avg = zoo::rollmean(feature, k = bin.size, fill = NA)) %>% ungroup()
+    return(df)
+})
 
-# #pick out TFs to highlight
-# files <- list.files(path = paste0("../output/", outName, "/"), pattern=".csv", all.files=FALSE,
-#                         full.names=T)
-# df.list <- lapply(files, read.csv, header = T)
-# df <- do.call(rbind, df.list)
+df <- do.call(rbind, mulit.plot) %>% na.omit() 
 
-# tfs <- read.csv("./metaData/allTFs_hg38.txt", col.names = "tfs")
-
-# df <- df[df$gene %in% tfs$tfs, ]
-
-# #gene expression by pseudotime
-# features <- c("PIR","IRF4","TFEC", "NFE2") #only works with 1 feat rn
-# colorBy <- "clusterID2_integrated.harmony" #seu metadata slot to color data by
-# plotBy <- "ptime" #pseudotime data to use on the x axis -- match with the name of a metadata slot
-# clus <- c("17","30")
-# rmZeros <- F #option to remove zero values - probablaly can keep it at false
-# bin.size <- 5
-
-# seu.obj.sub <- subset(seu.obj, clusterID2_integrated.harmony %in% clus)
-# mulit.plot <- lapply(features, function(x){
-#     df <- FetchData(object = seu.obj.sub, vars = c('cellSource', 'name', colorBy, plotBy, x), layer = "data")
-#     colnames(df)[3] <- "cluster"
-#     colnames(df)[5] <- "feature"
-
-#     if(rmZeros){
-#         df <- df[df[ ,"feature"] != 0, ]
-#     }
-
-#     df <- df %>% na.omit() %>% group_by(cluster) %>% mutate(feat = x,
-#                                                             ptime = (ptime - sort(ptime)[3]) / (rev(sort(ptime))[3] - sort(ptime)[3]) 
-#                                                            ) %>% arrange(ptime) %>% mutate(avg = zoo::rollmean(feature, k = bin.size, fill = NA)) %>% ungroup()
-#     return(df)
-# })
-
-# df <- do.call(rbind, mulit.plot) %>% na.omit() 
-
-# p <- ggplot(data = df, aes(x = ptime, y = avg, color = feat, linetype = cluster)) + geom_smooth(se = FALSE)  + labs(title = "TFs by time", x = "Pseudotime", y = "Log2 normalized count") + guides(colour = guide_legend(ncol = 3)) 
-# ggsave(paste0("../output/", outName, "/", outName, "_feats_byTime.png"), width = 7, height = 7)
+p <- ggplot(data = df, aes(x = ptime, y = avg, color = feat, linetype = cluster)) + theme_classic() +
+geom_smooth(se = FALSE)  + labs(x = "Pseudotime", y = "Log2 normalized count") + guides(colour = guide_legend(ncol = 3)) 
+ggsave(paste0("../output/", outName, "/", outName, "_feats_byTime.png"), width = 6, height = 4)
