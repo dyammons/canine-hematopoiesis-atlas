710dc3fdc9114e1e60b83724249eea74720c909a
dyammons <dyammons95@gmail.com>
2024-03-03 18:12:05 -0700

    Run hu-can integration using Manton ref
    This run integrated as planned and can be used
    moving forward with analysis.
    
    JOBID: 5217139

--- human_k9_comp.R
+++ human_k9_comp.R
@@ -98,13 +98,12 @@ rm(seu.list)
 gc()
 
 
-stop("This is the end for integration. Rest needs to be tested!")
-seu.obj <- readRDS("../output/s2/human_k9_S2.rds")
+# seu.obj <- readRDS("../output/s2/human_k9_S2.rds")
 #complete data visualization & save the RDS file
 message(paste0(Sys.time(), " INFO: data integration complete. compeleting dimension reduction and saving integrated object as a .rds file in ../s3/."))
 seu.obj <- dataVisUMAP(seu.obj = seu.obj, outDir = "../output/s3/", outName = "integrated_v4", 
                         final.dims = 45, final.res = 0.8, stashID = "clusterID", algorithm = 3, min.dist = 0.2, n.neighbors = 20,
-                        prefix = "RNA_snn_res.", assay = "RNA", reduction = "integrated",
+                        prefix = "integrated_snn_res.", assay = "integrated", reduction = "pca",
                         saveRDS = T, return_obj = T, returnFeats = T,
                         features = c("PTPRC", "CD3E", "CD8A", "GZMA", 
                                         "IL7R", "ANPEP", "FLT3", "HLA-DRA", 
@@ -116,7 +115,7 @@ gc()
 message(paste0(Sys.time(), " INFO: file saved. moving to plot hierchical clustering."))
 
 #reload in the integrated object
-seu.obj <- readRDS("../output/s3/integrated.harmony_res0.8_dims45_dist0.2_neigh20_S3.rds")
+# seu.obj <- readRDS("../output/s3/integrated_v4_res0.8_dims45_dist0.2_neigh20_S3.rds")
 
 #tag cell type labels with species labels to provide contrast
 seu.obj$cellSource <- ifelse(grepl("Manton", seu.obj$orig.ident), "Human", "Canine")    
@@ -124,7 +123,7 @@ seu.obj$type <- ifelse(grepl("Canine", seu.obj$cellSource), paste0("c_", seu.obj
 
 #extract data to plot
 metadata <- seu.obj@meta.data
-expression <- as.data.frame(t(as.matrix(seu.obj@assays$RNA@layers$data)))
+expression <- as.data.frame(t(as.matrix(seu.obj@assays$integrated@data)))
 expression$anno_merge <- seu.obj@meta.data[rownames(expression),]$type
 
 #get average expression by cluster
@@ -135,7 +134,7 @@ avg_expression$anno_merge <- NULL
 #complete heirchical clustering and plot the data using two different methods
 M <- (1- cor(t(avg_expression), method="pearson"))/2
 
-outfile <- paste("./output/", outName, "/", outName, "_phylo.png", sep = "")
+outfile <- paste("../output/", outName, "/", outName, "_phylo.png", sep = "")
 png(file = outfile, width=4000, height=4000, res=400)
 par(mfcol=c(1,1))
 hc <- hclust(as.dist(M),method="complete")
@@ -143,16 +142,15 @@ p <- plot.phylo(as.phylo(hc), type = "fan", cex = 0.8, no.margin = FALSE, tip.co
 dev.off()
 
 ggtree(as.phylo(hc), layout = "fan") + geom_tiplab() + NoLegend() + xlim(NA, 0.75)
-ggsave(paste("./output/", outName, "/", outName, "_ggTree.png", sep = ""), width = 9, height = 9) 
+ggsave(paste("../output/", outName, "/", outName, "_ggTree.png", sep = ""), width = 9, height = 9) 
 
 
 ### Use defining features to evaluate cell type gene signature overlap between species
 message(paste0(Sys.time(), " INFO: evlauting gene expression intersections between species."))
 
 #load in preprocessed data
-seu.obj.hu <- readRDS("../output/s3/integrated.harmony_res0.8_dims45_dist0.2_neigh20_S3.rds")
-seu.obj.dog <- seu.obj.k9
-rm(seu.obj.k9)
+seu.obj.hu <- readRDS("../output/s3/manton_ref_res0.8_dims45_dist0.2_neigh20_S3.rds")
+seu.obj.dog <- readRDS("../output/s3/allCellslabTransfer_S3.rds")
 
 seu.obj.hu[[hu.meta]] <- droplevels(seu.obj.hu[[hu.meta]])
 
@@ -174,138 +172,139 @@ vilnPlots(seu.obj = seu.obj.dog, inFile = NULL, groupBy = dog.meta, numOfFeats =
 
 ############ CODE BELOW IS UNTESTED ############
 
+stop()
 
-#read in the cell type gene lists
-dog.df <- read.csv("../output/viln/allCells_clean/allCells_clean_clusterID2_integrated.harmony_gene_list.csv") # need to convert these to human gene sybmols
-df <- as.data.frame(unique(dog.df$gene))
-colnames(df) <- "gene"
-geneConverts <- orthogene::convert_orthologs(gene_df = df,
-                                        gene_input = "gene", 
-                                        gene_output = "column", 
-                                        input_species = "dog",
-                                        output_species = "human",
-                                        non121_strategy = "drop_both_species") 
-dog.df <- dog.df %>% left_join(geneConverts, by = c("gene" = "input_gene"))
-dog.df <- na.omit(dog.df)
-human.df <- read.csv("../output/viln/manton/manton_bm_gene_list.csv")
+# #read in the cell type gene lists
+# dog.df <- read.csv("../output/viln/allCells_clean/allCells_clean_clusterID2_integrated.harmony_gene_list.csv") # need to convert these to human gene sybmols
+# df <- as.data.frame(unique(dog.df$gene))
+# colnames(df) <- "gene"
+# geneConverts <- orthogene::convert_orthologs(gene_df = df,
+#                                         gene_input = "gene", 
+#                                         gene_output = "column", 
+#                                         input_species = "dog",
+#                                         output_species = "human",
+#                                         non121_strategy = "drop_both_species") 
+# dog.df <- dog.df %>% left_join(geneConverts, by = c("gene" = "input_gene"))
+# dog.df <- na.omit(dog.df)
+# human.df <- read.csv("../output/viln/manton/manton_bm_gene_list.csv")
 
-#check the data quality
-dog.df %>% group_by(cluster) %>% summarize(nn = n()) %>% summarize(min = min(nn))
-human.df %>% group_by(cluster) %>% summarize(nn = n()) %>% summarize(min = min(nn))
+# #check the data quality
+# dog.df %>% group_by(cluster) %>% summarize(nn = n()) %>% summarize(min = min(nn))
+# human.df %>% group_by(cluster) %>% summarize(nn = n()) %>% summarize(min = min(nn))
 
-#calc the jaccard similarity index
-res <- lapply(unique(dog.df$cluster), function(x){
-    dog.list <- dog.df[dog.df$cluster == x, ] %>% .$gene
+# #calc the jaccard similarity index
+# res <- lapply(unique(dog.df$cluster), function(x){
+#     dog.list <- dog.df[dog.df$cluster == x, ] %>% .$gene
     
-    res_pre <- lapply(unique(human.df$cluster), function(y){
-        human.list <- human.df[human.df$cluster == y, ] %>% .$gene
+#     res_pre <- lapply(unique(human.df$cluster), function(y){
+#         human.list <- human.df[human.df$cluster == y, ] %>% .$gene
         
-        interSect <- length(intersect(human.list, dog.list)) 
-        uni <- length(human.list) + length(dog.list) - interSect 
-        JaccardIndex <- interSect/uni
-        names(JaccardIndex) <- x
+#         interSect <- length(intersect(human.list, dog.list)) 
+#         uni <- length(human.list) + length(dog.list) - interSect 
+#         JaccardIndex <- interSect/uni
+#         names(JaccardIndex) <- x
         
-        return(JaccardIndex)
-    })
+#         return(JaccardIndex)
+#     })
     
-    res_pre <- do.call(rbind, res_pre)
-    rownames(res_pre) <- unique(human.df$cluster)
-    return(res_pre)
+#     res_pre <- do.call(rbind, res_pre)
+#     rownames(res_pre) <- unique(human.df$cluster)
+#     return(res_pre)
     
-})
+# })
     
-res1 <- do.call(cbind, res)
+# res1 <- do.call(cbind, res)
 
-# #order the celltypes
-# rowTarg <- c("Endothelial","Fibroblast" ,"Osteoblast","Cycling osteoblast",
-#        "CD14_monocyte","NR4A3_Macrophage","TXNIP_Macrophage","FABP5_Macrophage",
-#        "IFN-TAM","Pre-OC","Mature-OC","pDC","cDC2","Mast",
-#        "CD8 T cell","CD4 T cell","IFN-sig T cell","NK cell",
-#        "Plasma cell","B cell")
-# colTarg <- c("Endothelial cell","Fibroblast","Osteoblast_1","Osteoblast_2","Osteoblast_3","Hypoxic_osteoblast",
-#          "IFN-osteoblast","Osteoblast_cycling","CD4+_TIM","CD4-_TIM","ANGIO_TAM","TAM_INT","TAM_ACT","LA-TAM_SPP2_hi",
-#          "LA-TAM_C1QC_hi","IFN-TAM","Cycling_OC","CD320_OC","Mature_OC","pDC","cDC1","cDC2","mregDC",
-#          "Mast cell","Neutrophil","CD8 T cell","CD4 T cell","T_IFN","T_cycling","NK","Plasma cell","B cell")
-# res1 <- res1[match(rowTarg, rownames(res1)),]        
-# res1 <- res1[ ,match(colTarg, colnames(res1))]   
+# # #order the celltypes
+# # rowTarg <- c("Endothelial","Fibroblast" ,"Osteoblast","Cycling osteoblast",
+# #        "CD14_monocyte","NR4A3_Macrophage","TXNIP_Macrophage","FABP5_Macrophage",
+# #        "IFN-TAM","Pre-OC","Mature-OC","pDC","cDC2","Mast",
+# #        "CD8 T cell","CD4 T cell","IFN-sig T cell","NK cell",
+# #        "Plasma cell","B cell")
+# # colTarg <- c("Endothelial cell","Fibroblast","Osteoblast_1","Osteoblast_2","Osteoblast_3","Hypoxic_osteoblast",
+# #          "IFN-osteoblast","Osteoblast_cycling","CD4+_TIM","CD4-_TIM","ANGIO_TAM","TAM_INT","TAM_ACT","LA-TAM_SPP2_hi",
+# #          "LA-TAM_C1QC_hi","IFN-TAM","Cycling_OC","CD320_OC","Mature_OC","pDC","cDC1","cDC2","mregDC",
+# #          "Mast cell","Neutrophil","CD8 T cell","CD4 T cell","T_IFN","T_cycling","NK","Plasma cell","B cell")
+# # res1 <- res1[match(rowTarg, rownames(res1)),]        
+# # res1 <- res1[ ,match(colTarg, colnames(res1))]   
        
-#plot the data
-png(file = paste0("../output/", outName, "/", outName, "_jaccard.png"), width=4000, height=4000, res=400)
-par(mfcol=c(1,1))         
-ht <- Heatmap(t(res1), #name = "mat", #col = col_fun,
-              name = "Jaccard similarity index",
-              cluster_rows = F,
-              row_title = "Canine cell types",
-              row_title_gp = gpar(fontsize = 24),
-              col=viridis(option = "magma",100),
-              cluster_columns = F,
-              column_title = "Human cell types",
-              column_title_gp = gpar(fontsize = 24),
-              column_title_side = "bottom",
-              column_names_rot = 45,
-              heatmap_legend_param = list(legend_direction = "horizontal", title_position = "topleft",  title_gp = gpar(fontsize = 16), 
-                                          labels_gp = gpar(fontsize = 8), legend_width = unit(6, "cm")),
-             )
-draw(ht, padding = unit(c(2, 12, 2, 5), "mm"),heatmap_legend_side = "top")
-dev.off()
+# #plot the data
+# png(file = paste0("../output/", outName, "/", outName, "_jaccard.png"), width=4000, height=4000, res=400)
+# par(mfcol=c(1,1))         
+# ht <- Heatmap(t(res1), #name = "mat", #col = col_fun,
+#               name = "Jaccard similarity index",
+#               cluster_rows = F,
+#               row_title = "Canine cell types",
+#               row_title_gp = gpar(fontsize = 24),
+#               col=viridis(option = "magma",100),
+#               cluster_columns = F,
+#               column_title = "Human cell types",
+#               column_title_gp = gpar(fontsize = 24),
+#               column_title_side = "bottom",
+#               column_names_rot = 45,
+#               heatmap_legend_param = list(legend_direction = "horizontal", title_position = "topleft",  title_gp = gpar(fontsize = 16), 
+#                                           labels_gp = gpar(fontsize = 8), legend_width = unit(6, "cm")),
+#              )
+# draw(ht, padding = unit(c(2, 12, 2, 5), "mm"),heatmap_legend_side = "top")
+# dev.off()
 
 
 
 
-### Transfer labels over from https://doi.org/10.1158/2159-8290.CD-22-1297
-hu.reference <- readRDS("../output/s3/manton_ref_res0.8_dims45_dist0.2_neigh20_S3.rds")
+# ### Transfer labels over from https://doi.org/10.1158/2159-8290.CD-22-1297
+# hu.reference <- readRDS("../output/s3/manton_ref_res0.8_dims45_dist0.2_neigh20_S3.rds")
 
-#transfer scArches_Cluster annotations
-ref.anchors <- FindTransferAnchors(reference = hu.reference, query = seu.obj, dims = 1:30,
-    reference.reduction = "pca", features = rownames(seu.obj)[rownames(seu.obj) %in% rownames(hu.reference)])
-predictions <- TransferData(anchorset = ref.anchors, refdata = hu.reference$mantonID, dims = 1:30)
-seu.obj <- AddMetaData(seu.obj, metadata = predictions)
-
-
-#plot the previously annotated labels (only for healthy cells)
-pi <- DimPlot(seu.obj, 
-              reduction = "umap.integrated.harmony", 
-              group.by = "predicted.id",
-              split.by = "predicted.id",
-              pt.size = 0.1,
-              ncol = 5,
-              repel = F
-)
-p <- formatUMAP(plot = pi) + NoLegend()
-ggsave(paste0("../output/", outName, "/", outName, "_mantonID_split_UMAP.png"), width = 14, height = 14)
+# #transfer scArches_Cluster annotations
+# ref.anchors <- FindTransferAnchors(reference = hu.reference, query = seu.obj, dims = 1:30,
+#     reference.reduction = "pca", features = rownames(seu.obj)[rownames(seu.obj) %in% rownames(hu.reference)])
+# predictions <- TransferData(anchorset = ref.anchors, refdata = hu.reference$mantonID, dims = 1:30)
+# seu.obj <- AddMetaData(seu.obj, metadata = predictions)
 
 
-#plot transfered labels
-pi <- DimPlot(seu.obj, 
-              reduction = "umap.integrated.harmony", 
-              group.by = "predicted.id",
-              pt.size = 0.1,
-              label = T,
-              label.box = T,
-              repel = T
-)
-p <- formatUMAP(plot = pi) + NoLegend()
-ggsave(paste0("../output/", outName, "/", outName, "_mantonID_transfer_UMAP.png"), width = 7, height = 7)
+# #plot the previously annotated labels (only for healthy cells)
+# pi <- DimPlot(seu.obj, 
+#               reduction = "umap.integrated.harmony", 
+#               group.by = "predicted.id",
+#               split.by = "predicted.id",
+#               pt.size = 0.1,
+#               ncol = 5,
+#               repel = F
+# )
+# p <- formatUMAP(plot = pi) + NoLegend()
+# ggsave(paste0("../output/", outName, "/", outName, "_mantonID_split_UMAP.png"), width = 14, height = 14)
 
 
-#transfer lsc_class annotations
-predictions <- TransferData(anchorset = ref.anchors, refdata = hu.reference$lsc_class, dims = 1:30)
-seu.obj <- AddMetaData(seu.obj, metadata = predictions)
+# #plot transfered labels
+# pi <- DimPlot(seu.obj, 
+#               reduction = "umap.integrated.harmony", 
+#               group.by = "predicted.id",
+#               pt.size = 0.1,
+#               label = T,
+#               label.box = T,
+#               repel = T
+# )
+# p <- formatUMAP(plot = pi) + NoLegend()
+# ggsave(paste0("../output/", outName, "/", outName, "_mantonID_transfer_UMAP.png"), width = 7, height = 7)
 
 
-seu.obj$predicted.id <- factor(seu.obj$predicted.id)
-#inspect data for proper import -- looks good
-pi <- DimPlot(seu.obj, 
-              reduction = "umap.integrated", 
-              group.by = "predicted.id",
-              split.by = "predicted.id",
-              pt.size = 0.1,
-              ncol = 3,
-              label = F,
-              label.box = F,
-              repel = F
-) + NoLegend()
-ggsave(paste0("../output/", outName, "/", outName, "_lsc_class_transfer_UMAP.png"), width = 12, height = 4)
+# #transfer lsc_class annotations
+# predictions <- TransferData(anchorset = ref.anchors, refdata = hu.reference$lsc_class, dims = 1:30)
+# seu.obj <- AddMetaData(seu.obj, metadata = predictions)
+
+
+# seu.obj$predicted.id <- factor(seu.obj$predicted.id)
+# #inspect data for proper import -- looks good
+# pi <- DimPlot(seu.obj, 
+#               reduction = "umap.integrated", 
+#               group.by = "predicted.id",
+#               split.by = "predicted.id",
+#               pt.size = 0.1,
+#               ncol = 3,
+#               label = F,
+#               label.box = F,
+#               repel = F
+# ) + NoLegend()
+# ggsave(paste0("../output/", outName, "/", outName, "_lsc_class_transfer_UMAP.png"), width = 12, height = 4)
 
 
 
